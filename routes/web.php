23103fede2be5a3e7a964807d888205b55135943
Carlo Angelo Fundar <carloangelofundar@gmail.com>
2026-03-13 13:48:43 +0800

    feat(user): add CRUD routes for user management in admin panel

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,15 @@ Route::middleware(['auth'])->group(function () {
 
     Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('/dashboard',  [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/users',      [UserController::class,      'index'])->name('users.index');
+
+        // Existing + New User Routes
+        Route::get('/users',              [UserController::class, 'index'])->name('users.index');
+        Route::get('/users/create',       [UserController::class, 'create'])->name('users.create');
+        Route::post('/users',             [UserController::class, 'store'])->name('users.store');
+        Route::get('/users/{user}/edit',  [UserController::class, 'edit'])->name('users.edit');
+        Route::put('/users/{user}',       [UserController::class, 'update'])->name('users.update');
+        Route::delete('/users/{user}',    [UserController::class, 'destroy'])->name('users.destroy');
+
         Route::get('/commuters',  [CommuterController::class,  'index'])->name('commuters.index');
         Route::get('/drivers',         [DriverController::class,       'index'])->name('drivers.index');
         Route::get('/organizations',              [OrganizationController::class, 'index'])->name('organizations.index');
