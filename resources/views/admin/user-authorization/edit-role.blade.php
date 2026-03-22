87e89e8bd563130faa3cb1b15a9e698dfa9efb17
MarjovicDEV <marjovicalejado1232@gmail.com>
2026-03-22 14:06:50 +0800

    feat(assignments): create driver assignments management page with search and assignment functionality

--- resources/views/admin/user-authorization/edit-role.blade.php
+++ resources/views/admin/user-authorization/edit-role.blade.php
@@ -18,6 +18,13 @@
 
 @section('content')
 
+    @if(session('error'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <i class="fas fa-exclamation-circle mr-1"></i> {{ session('error') }}
+            <button type="button" class="close" data-dismiss="alert"><span>&times;</span></button>
+        </div>
+    @endif
+
     {{-- Flash Messages --}}
     @if($errors->any())
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
