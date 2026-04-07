91c6c8d75c3dd56ce3296b5eb8245bfdf1e5ff38
Edriane0226 <edriane.bangonon26@gmail.com>
2026-04-07 23:39:39 +0800

    feat(waypoint): relationships for passenger starts, stops, and trip waypoints

--- app/Models/Waypoint.php
+++ app/Models/Waypoint.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Waypoint extends Model
@@ -21,4 +22,24 @@ class Waypoint extends Model
         'latitude' => 'float',
         'longitude' => 'float',
     ];
+
+    public function passengerStarts(): HasMany
+    {
+        return $this->hasMany(PassengerStart::class, 'waypoint_id');
+    }
+
+    public function passengerStops(): HasMany
+    {
+        return $this->hasMany(PassengerStop::class, 'waypoint_id');
+    }
+
+    public function tripStarts(): HasMany
+    {
+        return $this->hasMany(Trip::class, 'trip_start_waypoint_id');
+    }
+
+    public function tripEnds(): HasMany
+    {
+        return $this->hasMany(Trip::class, 'trip_end_waypoint_id');
+    }
 }
