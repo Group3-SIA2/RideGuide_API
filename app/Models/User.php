c14566adf7016aac3de925e1000495d2e3df303b
MarjovicDEV <marjovicalejado1232@gmail.com>
2026-05-10 01:52:05 +0800

    feat(User): add condition to roles relationship to exclude soft-deleted entries

--- app/Models/User.php
+++ app/Models/User.php
@@ -92,6 +92,7 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Role::class, 'user_role')
             ->using(UserRole::class)
+            ->wherePivotNull('deleted_at')
             ->withTimestamps();
     }
 
